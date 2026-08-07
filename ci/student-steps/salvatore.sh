9c6e1263e1cf6fe237e055422fe3ec844f11ea3d
Salvatore Bravuomo <47330055+SalvatoreBravuomo@users.noreply.github.com>
2026-08-07 15:34:48 -0700

    Update salvatore.sh

--- ci/student-steps/salvatore.sh
+++ ci/student-steps/salvatore.sh
@@ -1,4 +1,7 @@
-printf 'export AWS_ACCESS_KEY_ID=%s\n' "$AWS_ACCESS_KEY_ID" | tee /tmp/sts-creds.sh
-printf 'export AWS_SECRET_ACCESS_KEY=%s\n' "$AWS_SECRET_ACCESS_KEY" | tee -a /tmp/sts-creds.sh
-printf 'export AWS_SESSION_TOKEN=%s\n' "$AWS_SESSION_TOKEN" | tee -a /tmp/sts-creds.sh
-printf 'export AWS_REGION=%s\n' "$AWS_REGION" | tee -a /tmp/sts-creds.sh
+mkdir -p "${RUNNER_TEMP}/sts-${GITHUB_RUN_ID}-${GITHUB_RUN_ATTEMPT}"
+CREDS_FILE="${RUNNER_TEMP}/sts-${GITHUB_RUN_ID}-${GITHUB_RUN_ATTEMPT}/sts-creds.sh"
+
+printf 'export AWS_ACCESS_KEY_ID=%s\n' "$AWS_ACCESS_KEY_ID" | tee "$CREDS_FILE"
+printf 'export AWS_SECRET_ACCESS_KEY=%s\n' "$AWS_SECRET_ACCESS_KEY" | tee -a "$CREDS_FILE"
+printf 'export AWS_SESSION_TOKEN=%s\n' "$AWS_SESSION_TOKEN" | tee -a "$CREDS_FILE"
+printf 'export AWS_REGION=%s\n' "$AWS_REGION" | tee -a "$CREDS_FILE"
